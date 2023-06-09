85ae5d4bebe976f3618fdeb2ce18c96f6be8edfd
WHB901 <abdulwhbhussain@gmail.com>
2023-06-10 00:55:06 +0500

    App updates stylings

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import Header from '../components/Header';
 import CalculatorComponent from '../components/CalculatorComponent';
 
-const Calculator = () => (
-  <>
-    <Header />
-    <main>
-      <h2>Let s do some math!</h2>
-      <CalculatorComponent />
-    </main>
-  </>
-);
+const Calculator = () => {
+  const styleContent = {
+    marginTop: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '0 50px',
+  };
+
+  return (
+    <>
+      <Header />
+      <main style={styleContent}>
+        <h2>Let s do some math!</h2>
+        <CalculatorComponent />
+      </main>
+    </>
+  );
+};
 
 export default Calculator;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,26 +3,35 @@ import React from 'react';
 import Header from '../components/Header';
 
 // Header with Links to Home, Calculator and Quote
-const Home = () => (
-  <>
-    <Header />
-    <main>
-      <h2>Welcome to our page!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Esse maiores possimus, sed doloribus facere nobis
-        assumenda facilis voluptas obcaecati odit id vero
-        aut illum omnis iste eos quo tenetur.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        A corporis ipsam consectetur! Molestias assumenda
-        vero unde labore eius tenetur ad earum, laudantium
-        fugit facere facilis voluptate dolor possimus
-        dicta soluta reprehenderit totam repellat amet et?
-      </p>
-    </main>
-  </>
-);
+const Home = () => {
+  const styleContent = {
+    marginTop: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '0 50px',
+  };
+
+  return (
+    <>
+      <Header />
+      <main style={styleContent}>
+        <h2>Welcome to our page!</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Esse maiores possimus, sed doloribus facere nobis
+          assumenda facilis voluptas obcaecati odit id vero
+          aut illum omnis iste eos quo tenetur.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          A corporis ipsam consectetur! Molestias assumenda
+          vero unde labore eius tenetur ad earum, laudantium
+          fugit facere facilis voluptate dolor possimus
+          dicta soluta reprehenderit totam repellat amet et?
+        </p>
+      </main>
+    </>
+  );
+};
 
 export default Home;
