c738e3b5a1760d5b1fdf848f3054a970461646f6
WHB901 <abdulwhbhussain@gmail.com>
2023-06-10 14:23:58 +0500

    Good Mobile version

---
+++ src/components/utilities/devices.js
@@ -0,0 +1,19 @@
+const breakpoints = {
+  xs: '320px',
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+};
+
+const devices = {
+  xs: `(min-width: ${breakpoints.xs})`,
+  sm: `(min-width: ${breakpoints.sm})`,
+  md: `(min-width: ${breakpoints.md})`,
+  lg: `(min-width: ${breakpoints.lg})`,
+  xl: `(min-width: ${breakpoints.xl})`,
+  '2xl': `(min-width: ${breakpoints['2xl']})`,
+};
+
+export default devices;
